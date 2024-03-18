eefec29c08e8ab324af30a483f55ead508234735
khalidchoukhmane <choukhmane.khalid@gmail.com>
2024-03-18 02:09:09 +0000

    Adding Vue 3 and Bootstrap 5 and bootstrap icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import {createApp} from 'vue'
+import App from './vue/app.vue'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+
+createApp(App).use(bootstrap).mount("#app");
+
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h4 class="text-center mt-5">Hi You can use both vue 3 and bootstrap 5 here.</h4>
+        <div class="row">
+            <div class="col-6 mt-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">ERP</h5>
+                        <p class="card-text">This ia a ERP Web App</p>
+                        <a href="#" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mt-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">ERP</h5>
+                        <p class="card-text">This is a ERP Web App</p>
+                        <a href="#" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    name: "app"
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
